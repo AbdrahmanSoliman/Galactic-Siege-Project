6ebda8a77552f2f742bc6ec6c82673a07262f1d5
Abdelrahman A. Soliman <43556094+AbdrahmanSoliman@users.noreply.github.com>
2023-07-09 08:26:52 +0300

    lobby ui and created a lobby
    lobby ui and created a lobby

--- Assets/Scripts/Networking/RTSNetworkManager.cs
+++ Assets/Scripts/Networking/RTSNetworkManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Mirror;
@@ -8,17 +9,32 @@ public class RTSNetworkManager : NetworkManager
 {
     [SerializeField] private GameObject unitSpawnerPrefab = null;
     [SerializeField] private GameOverHandler gameoverHandlerPrefab = null;
+
+    public static event Action ClientOnConnected;
+    public static event Action ClientOnDisconnected;
+
+    public override void OnClientConnect()
+    {
+        base.OnClientConnect();
+        
+        ClientOnConnected?.Invoke();
+    }
+
+    public override void OnClientDisconnect()
+    {
+        base.OnClientDisconnect();
+
+        ClientOnDisconnected?.Invoke();
+    }
+
+
     public override void OnServerAddPlayer(NetworkConnectionToClient conn)
     {
         base.OnServerAddPlayer(conn);
 
         // Setting random team color for each player
         RTSPlayer player = conn.identity.GetComponent<RTSPlayer>();
-        player.SetTeamColor(new Color(Random.Range(0f,1f), Random.Range(0f,1f), Random.Range(0f,1f)));
-
-        GameObject unitSpawnerInstance = Instantiate(unitSpawnerPrefab, conn.identity.transform.position, conn.identity.transform.rotation);
-
-        NetworkServer.Spawn(unitSpawnerInstance, conn);
+        player.SetTeamColor(new Color(UnityEngine.Random.Range(0f,1f), UnityEngine.Random.Range(0f,1f), UnityEngine.Random.Range(0f,1f)));
     }
 
     public override void OnServerSceneChanged(string sceneName)
