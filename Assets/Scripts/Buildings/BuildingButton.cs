3e59a8efd74207a910f13ff8d8e61d683712975c
Abdelrahman A. Soliman <43556094+AbdrahmanSoliman@users.noreply.github.com>
2023-07-15 21:37:27 +0300

    click instead of drag to spawn a building
    You no longer have to drag a building from ui to spawn it on the ground, all you have to do is to click on building icon on ui and click whenever you want to place it on ground

--- Assets/Scripts/Buildings/BuildingButton.cs
+++ Assets/Scripts/Buildings/BuildingButton.cs
@@ -7,14 +7,14 @@ using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
-public class BuildingButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+public class BuildingButton : MonoBehaviour // IPointerDownHandler, IPointerUpHandler
 {
     [SerializeField] private Building building;
     [SerializeField] private Image imageIcon;
     [SerializeField] private TMP_Text priceText;
     [SerializeField] private LayerMask floorMask;
 
-    private Camera mainCamera;
+    [SerializeField] private Camera mainCamera;
     private BoxCollider boxCollider;
     private RTSPlayer player;
     private GameObject buildingPreviewInstance;
@@ -22,8 +22,6 @@ public class BuildingButton : MonoBehaviour, IPointerDownHandler, IPointerUpHand
 
     private void Start() 
     {
-        mainCamera = Camera.main;
-
         imageIcon.sprite = building.GetIcon();
         priceText.text = building.GetPrice().ToString();
 
@@ -38,50 +36,112 @@ public class BuildingButton : MonoBehaviour, IPointerDownHandler, IPointerUpHand
         if(buildingPreviewInstance == null) return;
 
         UpdateBuildingPreview();
+        PlaceBuilding();
     }
 
-
-    public void OnPointerDown(PointerEventData eventData)
+    public void Build() 
     {
-        if(eventData.button != PointerEventData.InputButton.Left) return;
-
         if(player.GetResources() < building.GetPrice()) return;
-        
+ 
         buildingPreviewInstance = Instantiate(building.GetBuildingPreview());
         buildingPreviewRenderer = buildingPreviewInstance.GetComponentInChildren<Renderer>();
-
+ 
         buildingPreviewInstance.SetActive(false);
-    }
-    public void OnPointerUp(PointerEventData eventData)
-    {
-        if(buildingPreviewInstance == null) return;
-
-        Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
-
-        if(Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, floorMask))
-        {
-            // Place the building
-
-            player.CmdTryPlaceBuilding(building.GetId(), hit.point);
-        }
-
-        Destroy(buildingPreviewInstance);
     }
 
     private void UpdateBuildingPreview()
     {
         Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
-        
-        if(!Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, floorMask)) return;
-
-        buildingPreviewInstance.transform.position = hit.point; // update building preview position based on mouse position
-
+ 
+        if (!Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, floorMask)) { return; }
+ 
+        buildingPreviewInstance.transform.position = hit.point;
+ 
         if(!buildingPreviewInstance.activeSelf)
         {
             buildingPreviewInstance.SetActive(true);
         }
-
+ 
         Color color = player.CanPlaceBuilding(boxCollider, hit.point) ? Color.green : Color.red;
+ 
         buildingPreviewRenderer.material.SetColor("_BaseColor", color);
     }
+
+    private void PlaceBuilding()
+    {
+        if(buildingPreviewInstance == null)
+            return;
+ 
+        if(Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+ 
+            if(Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, floorMask))
+            {
+                player.CmdTryPlaceBuilding(building.GetId(), hit.point);
+            }
+            
+            Destroy(buildingPreviewInstance);
+        }
+ 
+        if(Mouse.current.rightButton.wasPressedThisFrame)
+        {
+            Destroy(buildingPreviewInstance);
+        }
+    }
+
+    #region oldScript
+    // Older One -- Mouse Drag Instead of Click to Spawn a Building
+    // private void Update() 
+    // {
+    //     if(buildingPreviewInstance == null) return;
+
+    //     UpdateBuildingPreview();
+    // }
+
+
+    // public void OnPointerDown(PointerEventData eventData)
+    // {
+    //     if(eventData.button != PointerEventData.InputButton.Left) return;
+
+    //     if(player.GetResources() < building.GetPrice()) return;
+        
+    //     buildingPreviewInstance = Instantiate(building.GetBuildingPreview());
+    //     buildingPreviewRenderer = buildingPreviewInstance.GetComponentInChildren<Renderer>();
+
+    //     buildingPreviewInstance.SetActive(false);
+    // }
+    // public void OnPointerUp(PointerEventData eventData)
+    // {
+    //     if(buildingPreviewInstance == null) return;
+
+    //     Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+
+    //     if(Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, floorMask))
+    //     {
+    //         // Place the building
+
+    //         player.CmdTryPlaceBuilding(building.GetId(), hit.point);
+    //     }
+
+    //     Destroy(buildingPreviewInstance);
+    // }
+
+    // private void UpdateBuildingPreview()
+    // {
+    //     Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+        
+    //     if(!Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, floorMask)) return;
+
+    //     buildingPreviewInstance.transform.position = hit.point; // update building preview position based on mouse position
+
+    //     if(!buildingPreviewInstance.activeSelf)
+    //     {
+    //         buildingPreviewInstance.SetActive(true);
+    //     }
+
+    //     Color color = player.CanPlaceBuilding(boxCollider, hit.point) ? Color.green : Color.red;
+    //     buildingPreviewRenderer.material.SetColor("_BaseColor", color);
+    // }
+    #endregion
 }
